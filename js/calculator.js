c14b6f614a8b7716f974ed724392b0be4a56248f
rkfr <e.xbyvnqn@gmail.com>
2019-02-26 19:46:36 +0200

    created calculator`s markup

---
+++ js/calculator.js
@@ -0,0 +1,68 @@
+'use strict'
+
+class Calculator {
+	constructor(parentEl) {
+		//...
+		parentEl.append(this.createElements());
+		console.log(parentEl);
+	}
+
+	createElements() {
+		const calculator = document.createElement('div'),
+			display = document.createElement('div'),
+			numButtons = document.createElement('div'),
+			funcButtons = document.createElement('div'),
+
+		funcButtonsNames = [{plus:'+'}, {min:'-'}, {equ:'='}, {dot:'.'}, {mul:'*'}, {div:'/'}, {mod:'%'}];
+
+		calculator.classList.add('calculator');
+		display.classList.add('calculator__display');
+		numButtons.classList.add('calculator__num-buttons');
+		funcButtons.classList.add('calculator__func-buttons');
+
+		for(let i = 0; i < 10; i++) {
+			const numButton = document.createElement('div');
+			numButton.classList.add('calculator__num-button');
+			numButton.innerText = i;
+			numButtons.append(numButton);
+		}
+
+		funcButtonsNames.forEach(el => {
+			const funcButton = document.createElement('div');
+
+			for (let key in el) {
+				funcButton.classList.add(`calculator__func-button`, `func-button__${key}`);
+				funcButton.innerText = el[key];
+			}
+
+			funcButtons.append(funcButton);
+		});
+
+		calculator.append(display, numButtons, funcButtons);
+		return calculator;
+	}
+}
+
+const parentEl = document.querySelector('.nav__item_calc');
+const calculator = new Calculator(parentEl);
+// calculator.createElements();
+
+
+// <div class="calculator">
+// 	<div class="calculator__display"></div>
+// 	<div class="calculator__num-buttons">
+// 		<div class="calculator__num-button">1</div>
+// 		<div class="calculator__num-button">2</div>
+// 		<div class="calculator__num-button">3</div>
+// 		<div class="calculator__num-button">4</div>
+// 		<div class="calculator__num-button">5</div>
+// 		<div class="calculator__num-button">6</div>
+// 		<div class="calculator__num-button">7</div>
+// 		<div class="calculator__num-button">8</div>
+// 		<div class="calculator__num-button">9</div>
+// 		<div class="calculator__num-button">0</div>
+// 	</div>
+// 	<div class="calculator__func-buttons">
+// 		<div class="calculator__funct-button"></div>
+// 	</div>
+// </div>
